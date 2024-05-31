f931135ed04584a58c20d318941f116e17fb3c47
Borzixx <tim.barzashvili@gmail.com>
2024-05-31 23:22:46 +0200

    Changed the usage of sensetive data, put it into the .env file

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import telebot
 from telebot import types
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 print('Here we go')
 
-bot = telebot.TeleBot('6784091371:AAFKg602ndv0ZLs20oPG4gzgrDl7jQicR8g')
+bot = telebot.TeleBot(getenv('BOT_ID'))
 
-user_state = {198781641: "0", 745178113: "0", 596747110: "0", 701101736: "0", 542008688: "0"}
+user_state = {int(getenv('ID_OF_DIMA')): "0", int(getenv('ID_OF_DAVID')): "0", int(getenv('ID_OF_VLAD')): "0", int(getenv('ID_OF_TYMOFII')): "0", int(getenv('ID_OF_MAX')): "0"}
 
 def menu(message):
     user_id = message.chat.id
@@ -84,40 +88,35 @@ def sending(message):
     user_id = message.chat.id
     user_state[user_id] = "0"
     if message.text == 'Написати боту' or message.text == 'napisać botu' or message.text == 'schreibe dem Bot' or message.text == 'écrire un bot':
-        # id bota
-        bot.send_message(701101736, "Бот лох\nВід " + message.from_user.username)
+        bot.send_message(int(getenv('ID_OF_TYMOFII')), "Бот лох\nВід " + message.from_user.username)
         gif = 'gif/bot_govoryt.mp4'
         with open(gif, 'rb') as gif:
             bot.send_document(message.chat.id, gif)
         bot.send_message(message.chat.id, 'Delivered')
         menu(message)
     elif message.text == 'Написати нубу' or message.text == 'napisać nubu' or message.text == 'schreibe an Noob' or message.text == 'écrire à noob':
-        # id bota(have to be noob)
-        bot.send_message(701101736, "Нуб лох\nВід " + message.from_user.username)
+        bot.send_message(int(getenv('ID_OF_DIMA')), "Нуб лох\nВід " + message.from_user.username)
         gif = 'gif/nub_vahui.MP4'
         with open(gif, 'rb') as gif:
             bot.send_document(message.chat.id, gif)
         bot.send_message(message.chat.id, 'Delivered')
         menu(message)
     elif message.text == 'Написати шнюку' or message.text == 'napisać szniuku' or message.text == 'schreib dem Schnatz' or message.text == 'écrire au vif d\'or':
-        # id shnuka 542008688
-        bot.send_message(542008688, "Шнюк лох\nВід " + message.from_user.username)
+        bot.send_message(int(getenv('ID_OF_MAX')), "Шнюк лох\nВід " + message.from_user.username)
         gif = 'gif/shnyuk_loh.mp4'
         with open(gif, 'rb') as gif:
             bot.send_document(message.chat.id, gif)
         bot.send_message(message.chat.id, 'Delivered')
         menu(message)
     elif message.text == 'Написати блюю' or message.text == 'napisać bluju' or message.text == 'schreibe an Blau' or message.text == 'écrire pour vomir':
-        # id shnuka(have to be bluy)
-        bot.send_message(701101736, "Блюй лох\nВід " + message.from_user.username)
+        bot.send_message(int(getenv('ID_OF_DAVID')), "Блюй лох\nВід " + message.from_user.username)
         gif = 'gif/bluy_i_bot.MP4'
         with open(gif, 'rb') as gif:
             bot.send_document(message.chat.id, gif)
         bot.send_message(message.chat.id, 'Delivered')
         menu(message)
     elif message.text == 'Написати вацкі' or message.text == 'napisać wacky' or message.text == 'Schreiben Sie an Vatsky' or message.text == 'écrire à Vatsky':
-        # id shnuka(have to be bluy)
-        bot.send_message(701101736, "Вацкі лох\nВід " + message.from_user.username)
+        bot.send_message(int(getenv('ID_OF_VLAD')), "Вацкі лох\nВід " + message.from_user.username)
         gif = 'gif/vlad_sose.mp4'
         with open(gif, 'rb') as gif:
             bot.send_document(message.chat.id, gif)
@@ -142,12 +141,6 @@ def get_text_messages(message):
     user_id = message.chat.id
     if user_state[user_id] == "1":
         language(message)
-        # else:
-        #     print('refutado 1')
-        #     refutado = 'gif/tony-stark-court.mp4'
-        #     with open(refutado, 'rb') as refutado:
-        #         bot.send_document(message.from_user.id, refutado)
-        #     bot.send_message(message.from_user.id, message.from_user.username + ' додік')
     elif user_state[user_id] == "2":
         sending(message)
     else:
